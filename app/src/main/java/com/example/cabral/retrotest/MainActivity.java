82790a80ccf582a8b38b5753fd9149030bf34db8
digicabral <digicabral@gmail.com>
2017-11-19 17:49:27 -0200

    ListView

--- app/src/main/java/com/example/cabral/retrotest/MainActivity.java
+++ app/src/main/java/com/example/cabral/retrotest/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.cabral.retrotest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
@@ -16,12 +19,14 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
 
     TextView txtRepos;
+    ListView lv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtRepos = (TextView)findViewById(R.id.txtRepositorio);
+        //txtRepos = (TextView)findViewById(R.id.txtRepositorio);
+        lv = (ListView) findViewById(R.id.lv);
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://api.github.com")
                 .addConverterFactory(GsonConverterFactory.create())
@@ -36,11 +41,20 @@ public class MainActivity extends AppCompatActivity {
 
                 // Pega a lista de repositorios no corpo da requisição.
                 List<Repo> reposResponse = response.body();
+                Log.i("reposResponse", reposResponse.toString());
                 // Percorre a lista e exibe o nome de cada repositório.
+                List<String> repoList = new ArrayList<String>();
                 for (Repo user : reposResponse){
                     Log.i("GitHubServices", user.getmName());
-                    txtRepos.setText(user.getmName());
+                    repoList.add(user.getmName());
+                    //lv.(user.getmName());
                 }
+                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                        MainActivity.this,
+                        android.R.layout.simple_list_item_1,
+                        repoList
+                );
+                lv.setAdapter(arrayAdapter);
             }
             @Override
             public void onFailure(Call<List<Repo>> call, Throwable t) {
